3d95c4d0a5375e07c31e875e6ea096ef61e53cce
reubence <reuben.rapose@gmail.com>
2024-06-26 21:24:55 +0530

    refactor: update HeroSection and Marquee styling

--- components/landing/hero-section.tsx
+++ components/landing/hero-section.tsx
@@ -13,8 +13,8 @@ import Link from "next/link";
 import { useLenis } from "lenis/react";
 
 export default function HeroSection() {
-  const ref = useRef(null);
-  const inView = useInView(ref, { once: true, margin: "-100px" });
+  // const ref = useRef(null);
+  // const inView = useInView(ref, { margin: "-100px" });
   const lenis = useLenis(({ scroll }) => {
     // called every scroll
   });
@@ -54,13 +54,11 @@ export default function HeroSection() {
         </Link>
       </div>
       <div
-        ref={ref}
+        // ref={ref}
         className="z-0 relative mt-[8rem] animate-fade-up opacity-0 [--animation-delay:400ms] [perspective:2000px] after:absolute after:inset-0 after:z-10 after:[background:linear-gradient(to_top,hsl(var(--background))_30%,transparent)]"
       >
         <div
-          className={`before:animate-image-glow absolute h-screen w-full z-0 before:absolute before:bottom-1/2 before:left-0 before:top-0 before:h-full before:w-full before:opacity-0 before:[filter:blur(180px)] before:[background-image:linear-gradient(to_bottom,var(--color-one),var(--color-one),transparent_40%)] ${
-            inView ? "before:animate-image-glow" : ""
-          }`}
+          className={`before:animate-image-glow absolute h-screen w-full z-0 before:absolute before:bottom-1/2 before:left-0 before:top-0 before:h-full before:w-full before:opacity-0 before:[filter:blur(180px)] before:[background-image:linear-gradient(to_bottom,var(--color-one),var(--color-one),transparent_40%)]`}
         >
           {/* <BorderBeam
             size={200}
