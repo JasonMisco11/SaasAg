7d6cc1d160bb2754dcd302c48c8778e5fe14c57d
Jason Asamoah <JasonAs@stlghana.com>
2025-12-29 17:00:18 +0000

    layout fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
-import './global.css'
-
+import './global.css';
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
